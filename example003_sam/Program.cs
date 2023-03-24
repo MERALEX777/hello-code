33a5820baf9e9a156dd0456aa98c9bf421c66a7e
meralex <meralex85@mail.ru>
2023-03-24 21:18:07 +0300

    рандомные числа

--- example003_sam/Program.cs
+++ example003_sam/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 2;
-int result =(numberA + numberB);
-Console.WriteLine(result);
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
+
 
